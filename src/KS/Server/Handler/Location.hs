868bf59616761df56b768472430acb247f3ba8df
Dino Morelli <dino@ui3.info>
2015-11-08 17:50:26 +0000

    Rewrote by_loc query code to be MUCH simpler
    This is part of a database redesign we're involved in. This will
    remove the need to do most (all?) of the aggregate pipeline queries
    in code.
    
    Also rewrote parseLngLat in terms of Data.Text.split, it's much
    simpler now.
    
    Also added some INFO level logging of the by_loc query string
    parameters.

--- src/KS/Server/Handler/Location.hs
+++ src/KS/Server/Handler/Location.hs
@@ -6,80 +6,49 @@
 module KS.Server.Handler.Location ( handler )
    where
 
-import Control.Arrow ( (&&&) )
 import Data.Aeson.Bson ( toAeson )
 import qualified Data.Text as T
 import Database.MongoDB hiding ( options )
+import Text.Printf ( printf )
 import Web.Scotty ( ActionM, json, param, rescue )
 
 import KS.Server.Config
---import KS.Server.Log
+import KS.Server.Log
+
+
+-- Default query distance in meters
+defaultDistance :: Double
+defaultDistance = 800
 
 
 handler :: MongoConf -> Pipe -> ActionM ()
 handler mc pipe = do
    pt <- parseLngLat <$> param "pt"
-   --liftIO $ debugM lname $ "pt = " ++ show pt
-   dist <- param "dist" `rescue` (return . const 800)
-   --liftIO $ debugM lname $ "dist = " ++ show dist
+   dist <- param "dist" `rescue` (return . const defaultDistance)
 
-   let geoNear =
-         [ "$geoNear" =:
-            [ "near" =:
-               [ "type" =: ("Point" :: T.Text)
-               , "coordinates" =: pt
-               ]
-            , "distanceField" =: ("dist.calculated" :: T.Text)
-            , "maxDistance" =: (dist :: Double)
-            , "includeLocs" =: ("place.location" :: T.Text)
-            , "num" =: (5000 :: Double)
-            , "spherical" =: True
-            ]
-        ]
+   liftIO $ infoM lname $ printf "by_loc query pt: %s, dist: %f"
+      (show pt) dist
 
-   let dateSort = ["$sort" =: ["inspection.date" =: (-1 :: Int)]]
-
-   let group' =
-         [ "$group" =:
-            [ "_id" =: ("$place.place_id" :: T.Text)
-            , "last_inspection" =:
-               [ "$first" =:
-                  [ "_id" =: ("$_id" :: T.Text)
-                  , "doctype" =: ("$doctype" :: String)
-                  , "inspection" =: ("$inspection" :: T.Text)
-                  , "place" =: ("$place" :: T.Text)
-                  ]
-               ]
-            , "dist" =: ["$max" =: ("$dist" :: T.Text)]
-            ]
+   ds <- access pipe slaveOk (database mc) $ runCommand (
+      [ "geoNear" =: ("inspections" :: T.Text)
+      , "near" =:
+         [ "type" =: ("Point" :: T.Text)
+         , "coordinates" =: pt
          ]
+      , "spherical" =: True
+      , "limit" =: (5000 :: Int)
+      , "maxDistance" =: dist
+      ])
 
-   let sort' = ["$sort" =: ["dist" =: (1 :: Int)]]
+   -- This one returns everything we get from mongo
+   --json . toAeson $ ds
 
-   let pipeline = [geoNear, dateSort, group', sort']
+   -- Just the inspections
+   --json . map toAeson . map (at "obj") . (at "results") $ ds
 
-   ds <- access pipe slaveOk (database mc) $ aggregate "inspections" pipeline
-
-   -- This one will includ the (odd) _id and the dist from the group
-   -- We'll need this later
-   --json . map toAeson $ ds
-
-   -- This one will return only the traditional inspection record
-   json . map toAeson . map (at "last_inspection") $ ds
+   -- The inspections and their distances
+   json . map toAeson . (at "results") $ ds
 
 
-{- Parse point data from query strings
-
-   from this: "75.1,-36.2"  (Text, similar to String)
-   to this:   [75.1, -36.2] ([Double], a List of Double)
--}
 parseLngLat :: T.Text -> [Double]
-parseLngLat t = [toDouble lng, toDouble lat]
-   where
-      -- Split on the comma, not including the comma
-      -- T.Text -> (T.Text, T.Text)
-      (lng, lat) = (T.takeWhile p &&& T.tail . T.dropWhile p) t
-      p = (/= ',')
-
-      toDouble :: T.Text -> Double
-      toDouble = read . T.unpack
+parseLngLat = map (read . T.unpack) . (T.split (== ','))
