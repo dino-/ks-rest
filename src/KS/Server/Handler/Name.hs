4c6c808a39dfd3f5b68454026e4354d0968843d4
Dino Morelli <dino@ui3.info>
2015-11-09 01:48:24 +0000

    Finished up by_name code
    Now logging the query string parameters too.

--- src/KS/Server/Handler/Name.hs
+++ src/KS/Server/Handler/Name.hs
@@ -10,12 +10,15 @@ import KS.Data.BSON ( bsonToDoc )
 import Web.Scotty ( ActionM, json, param )
 
 import KS.Server.Config
---import KS.Server.Log
+import KS.Server.Log
 
 
 handler :: MongoConf -> Pipe -> ActionM ()
 handler mc pipe = do
    regex' <- param "regex"
-   ds <- access pipe slaveOk (database mc) $ do
-      rest =<< find (select ["place.name" =: Regex (regex' :: T.Text) "i"] "inspections")
+
+   liftIO $ infoM lname $ "by_name received, regex: " ++ (T.unpack regex')
+
+   ds <- access pipe slaveOk (database mc) $ rest =<<
+      find (select ["place.name" =: Regex (regex' :: T.Text) "i"] "inspections")
    json . map bsonToDoc $ ds
