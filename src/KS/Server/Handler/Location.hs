dce042e5a1bd8cce22f2cc4bb5947cc413fb0acc
Dino Morelli <dino@ui3.info>
2015-11-09 03:28:21 +0000

    Added a min_score query param to by_loc
    This will perform the geo query with optional filtering for a
    minimum food rating score. Defaults to 0.0

--- src/KS/Server/Handler/Location.hs
+++ src/KS/Server/Handler/Location.hs
@@ -19,10 +19,15 @@ defaultDistance :: Double
 defaultDistance = 800
 
 
+defaultMinScore :: Double
+defaultMinScore = 0.0
+
+
 handler :: MongoConf -> Pipe -> ActionM ()
 handler mc pipe = do
    pt <- parseLngLat <$> param "pt"
    dist <- param "dist" `rescue` (return . const defaultDistance)
+   minScore <- param "min_score" `rescue` (return . const defaultMinScore)
 
    liftIO $ infoM lname $ printf "by_loc received, pt: %s, dist: %f"
       (show pt) dist
@@ -36,6 +41,7 @@ handler mc pipe = do
       , "spherical" =: True
       , "limit" =: (5000 :: Int)
       , "maxDistance" =: dist
+      , "query" =: [ "inspection.score" =: [ "$gte" =: minScore ] ]
       ])
 
    -- This one returns everything we get from mongo
