1d973ea5e4134489b742aa35bb4fcd75ed79ded5
Dino Morelli <dino@ui3.info>
2015-11-29 21:59:39 +0000

    Changed ping API call to version
    This will perform the same job and is much more useful.

---
+++ src/KS/Server/Handler/Version.hs
@@ -0,0 +1,26 @@
+-- License: BSD3 (see LICENSE)
+-- Author: Dino Morelli <dino@ui3.info>
+
+module KS.Server.Handler.Version ( handler )
+   where
+
+import Data.Aeson.Bson ( toAeson )
+import Data.Version ( showVersion )
+import Database.MongoDB hiding ( options )
+import Paths_ks_server ( version )
+import Web.Scotty ( ActionM, json )
+
+
+ksServerVersion, ksAPIVersion :: String
+
+-- This comes from the .cabal file
+ksServerVersion = showVersion version
+
+ksAPIVersion = "1.0"
+
+
+handler :: ActionM ()
+handler = json . toAeson $
+   [ "ks_server_version" =: ksServerVersion
+   , "ks_api_version" =: ksAPIVersion
+   ]
--- src/ks-server.hs
+++ src/ks-server.hs
@@ -5,13 +5,14 @@ import Data.Version ( showVersion )
 import Database.MongoDB hiding ( options )
 import Paths_ks_server ( version )
 import System.Environment ( getArgs )
-import Web.Scotty ( get, middleware, scotty, text )
+import Web.Scotty ( get, middleware, scotty )
 
 import KS.Server.Config
 import qualified KS.Server.Handler.Location as Location
 import qualified KS.Server.Handler.Name as Name
 import qualified KS.Server.Handler.PlaceId as PlaceId
 import qualified KS.Server.Handler.Source as Source
+import qualified KS.Server.Handler.Version as Version
 import KS.Server.Log
 
 
@@ -44,7 +45,7 @@ main = do
       get  "/inspections/by_name"               $ Name.handler mc pipe
       get  "/inspections/by_placeid/:placeid"   $ PlaceId.handler mc pipe
       get  "/inspections/by_source/:criteria"   $ Source.handler mc pipe
-      get  "/ping"                              $ text "pong\n"
+      get  "/version"                           $ Version.handler
 
    {- These never execute, is that bad? Can do something threaded if necessary
 
