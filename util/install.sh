387490eb697145807208fada97ac143ae11b0e2e
Dino Morelli <dino@ui3.info>
2016-01-22 19:31:52 +0000

    Install script now creates the symlink
    Also much better help and error checking.

--- util/install.sh
+++ util/install.sh
@@ -2,11 +2,47 @@
 
 set -e
 
+installDir="/opt/ksnitch"
 sandboxDir="../cabal-sandbox"
-project=ks-server
+project="ks-server"
 version=$(perl -n -e '/^version: (.*)/ && print $1' ${project}.cabal)
+
+
+basename=$(basename $0)
+
+function usage {
+   cat <<USAGE
+$basename - Build and install $project
+
+usage:
+   $basename [OPTIONS]
+
+options:
+   -h, --help  This help info
+   -l, --link  Create symlink in $installDir
+
+USAGE
+
+   exit 0
+}
+
+
+dolink=false
+
+case "$1" in
+   -h|--help) usage ;;
+   -l|--link) dolink=true ;;
+esac
+
 
 cabal clean
 cabal sandbox init --sandbox=$sandboxDir
 cabal install --only-dep
-cabal install --prefix=/opt/ksnitch/${project}-${version} --datasubdir=.
+cabal install --prefix=$installDir/${project}-${version} --datasubdir=.
+
+if $dolink
+then
+   cd $installDir
+   rm -f $project
+   ln -s ${project}-${version} $project
+fi
