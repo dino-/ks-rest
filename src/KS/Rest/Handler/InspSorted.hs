8c975369846176fde56077bdd0edd68acf61f46c
Dino Morelli <dino@ui3.info>
2016-02-06 16:08:56 +0000

    Moved two functions from Util to the specific handler where they are used

--- src/KS/Rest/Handler/InspSorted.hs
+++ src/KS/Rest/Handler/InspSorted.hs
@@ -6,20 +6,23 @@ module KS.Rest.Handler.InspSorted
    where
 
 import           Control.Monad.Trans ( liftIO )
-import           Control.Monad.Trans.Either ( EitherT )
+import           Control.Monad.Trans.Either ( EitherT, left )
 import           Data.Bson.Generic ( fromBSON )
+import qualified Data.ByteString.Lazy.Char8 as C
 import           Data.Maybe ( catMaybes )
 import qualified Data.Text as T
 import           Database.MongoDB hiding ( Value, options )
-import           Servant ( ServantErr )
+import           Servant
+                  ( ServantErr (errBody)
+                  , err400
+                  )
 import           Text.Printf ( printf )
 
 import qualified KS.Data.Document as D
 import           KS.Rest.APIKey ( akRead )
 import           KS.Rest.Config ( Config (mongoConf), MongoConf (database) )
 import           KS.Rest.Log ( infoM, lineM, lname )
-import           KS.Rest.Util ( metersToEarthRadians, parseSortParam
-                  , requiredParam, verifyAPIKey )
+import           KS.Rest.Util ( requiredParam, verifyAPIKey )
 
 
 defaultLimit :: Limit
@@ -69,3 +72,48 @@ handler
    liftIO $ infoM lname $ printf "Retrieved %d inspections" $ length ds
 
    return $ catMaybes . map fromBSON $ ds
+
+
+metersToEarthRadians :: Double -> Double
+metersToEarthRadians m = (mToKm m) / earthRadiansPerKm
+   where
+      -- Convert meters to kilometers
+      mToKm m' = m' * 0.001
+
+      -- Number of radians on the Earth coordinate system in
+      -- a kilometer
+      earthRadiansPerKm = 6378.1
+
+      -- Number of radians on the Earth coordinate system in
+      -- a mile. Not used, but in case we have a future need.
+      --earthRadiansPerMile = 3963.2
+
+
+parseSortParam :: T.Text -> EitherT ServantErr IO Order
+parseSortParam paramValue = do
+   ordering <- convertOrdering $ T.take 1 paramValue
+   field <- verifyField $ T.drop 1 paramValue
+   return $ [ (T.concat ["inspection.", field]) =: ordering ]
+
+   where
+      convertOrdering :: T.Text -> EitherT ServantErr IO Int
+      convertOrdering "+" = return 1
+      convertOrdering "-" = return (-1)
+      convertOrdering o   = left $ err400 { errBody = C.concat
+         [ "Cannot parse sort parameter because we got sort ordering '"
+         , (C.pack . T.unpack $ o)
+         , "' but expected '+' or '-'"
+         ] }
+
+      validSortFields :: [T.Text]
+      validSortFields = ["date", "score"]
+
+      verifyField :: T.Text -> EitherT ServantErr IO T.Text
+      verifyField field
+         | field `elem` validSortFields = return field
+         | otherwise = left $ err400 { errBody = C.concat
+            [ "Cannot parse sort parameter because we got sort field '"
+            , (C.pack . T.unpack $ field)
+            , "' but expected one of "
+            , (C.pack . show $ validSortFields)
+            ] }
--- src/KS/Rest/Util.hs
+++ src/KS/Rest/Util.hs
@@ -3,8 +3,6 @@
 
 module KS.Rest.Util
    ( coll_inspections_all, coll_inspections_recent
-   , metersToEarthRadians
-   , parseSortParam
    , requiredParam
    , verifyAPIKey
    )
@@ -12,10 +10,9 @@ module KS.Rest.Util
 
 import           Control.Monad.Trans ( liftIO )
 import           Control.Monad.Trans.Either ( EitherT, left )
-import qualified Data.ByteString.Lazy.Char8 as C
+import           Data.ByteString.Lazy.Char8 ( pack )
 import           Data.ByteString.Lazy ( ByteString, concat )
-import qualified Data.Text as T
-import           Database.MongoDB ( Order, Collection, (=:) )
+import           Database.MongoDB ( Collection )
 import           Prelude hiding ( concat )
 import           Servant
                   ( ServantErr (errBody)
@@ -50,53 +47,8 @@ verifyAPIKey conf perms keyValue =
    case (AK.verifyAPIKey perms (apiKeys conf) keyValue) of
       Left msg -> do
          liftIO $ noticeM lname msg
-         left $ err401 { errBody = C.pack msg}
+         left $ err401 { errBody = pack msg}
       Right _  -> do
          liftIO $ infoM lname
             $ "Successful authentication with API key " ++ keyValue
          return keyValue
-
-
-metersToEarthRadians :: Double -> Double
-metersToEarthRadians m = (mToKm m) / earthRadiansPerKm
-   where
-      -- Convert meters to kilometers
-      mToKm m' = m' * 0.001
-
-      -- Number of radians on the Earth coordinate system in
-      -- a kilometer
-      earthRadiansPerKm = 6378.1
-
-      -- Number of radians on the Earth coordinate system in
-      -- a mile. Not used, but in case we have a future need.
-      --earthRadiansPerMile = 3963.2
-
-
-parseSortParam :: T.Text -> EitherT ServantErr IO Order
-parseSortParam paramValue = do
-   ordering <- convertOrdering $ T.take 1 paramValue
-   field <- verifyField $ T.drop 1 paramValue
-   return $ [ (T.concat ["inspection.", field]) =: ordering ]
-
-   where
-      convertOrdering :: T.Text -> EitherT ServantErr IO Int
-      convertOrdering "+" = return 1
-      convertOrdering "-" = return (-1)
-      convertOrdering o   = left $ err400 { errBody = C.concat
-         [ "Cannot parse sort parameter because we got sort ordering '"
-         , (C.pack . T.unpack $ o)
-         , "' but expected '+' or '-'"
-         ] }
-
-      validSortFields :: [T.Text]
-      validSortFields = ["date", "score"]
-
-      verifyField :: T.Text -> EitherT ServantErr IO T.Text
-      verifyField field
-         | field `elem` validSortFields = return field
-         | otherwise = left $ err400 { errBody = C.concat
-            [ "Cannot parse sort parameter because we got sort field '"
-            , (C.pack . T.unpack $ field)
-            , "' but expected one of "
-            , (C.pack . show $ validSortFields)
-            ] }
