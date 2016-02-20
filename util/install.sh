1ec44bf0e4add6d4a95d29043162cfb6b54cbda7
Dino Morelli <dino@ui3.info>
2016-02-20 17:02:06 +0000

    Switched to using GNU getopt for args parsing

--- util/install.sh
+++ util/install.sh
@@ -7,8 +7,8 @@ sandboxDir="../cabal-sandbox"
 project="ks-rest"
 version=$(perl -n -e '/^version: (.*)/ && print $1' ${project}.cabal)
 
-
 basename=$(basename $0)
+
 
 function usage {
    cat <<USAGE
@@ -18,21 +18,34 @@ usage:
    $basename [OPTIONS]
 
 options:
-   -h, --help  This help info
    -l, --link  Create symlink in $installDir
+   -h, --help  This help info
 
 USAGE
-
-   exit 0
 }
 
 
-dolink=false
+# arg parsing
 
-case "$1" in
-   -h|--help) usage ;;
-   -l|--link) dolink=true ;;
-esac
+getoptResults=$(getopt -o lh --long link,help -n $basename -- "$@")
+
+if [ $? != 0 ]; then usage; exit 1; fi
+
+# Note the quotes around '$getoptResults': they are essential!
+eval set -- "$getoptResults"
+
+optLink=false
+optHelp=false
+
+while true ; do
+   case "$1" in
+      -l|--link) optLink=true; shift;;
+      -h|--help) optHelp=true; shift;;
+      --) shift; break;;
+   esac
+done
+
+if $optHelp; then usage; exit 0; fi
 
 
 cabal clean
@@ -40,7 +53,7 @@ cabal sandbox init --sandbox=$sandboxDir
 cabal install --only-dep
 cabal install --prefix=$installDir/${project}-${version} --datasubdir=.
 
-if $dolink
+if $optLink
 then
    cd $installDir
    rm -f $project
