aa6ee663a97acf9a6073dc23e806857f169fc120
Dino Morelli <dino@ui3.info>
2016-04-07 22:36:08 +0000

    Updated stress test script
    This was written before we had API keys, now will use them.

--- resources/bin/stress.sh
+++ resources/bin/stress.sh
@@ -2,11 +2,14 @@
 
 # Script to stress-test hitting our server for by_placeid results
 
-curlArgs="-s --connect-timeout 5"
+apiKey=$(<$HOME/.config/ksnitch/test-apikey)
+
+#curlArgs="-s --connect-timeout 5"
+curlArgs="--connect-timeout 1"
 
 placeId=${1:?"Please supply a Google Places ID"}
 
-url="http://localhost:8001/inspections/by_placeid/$placeId"
+url="http://ksdev.honuapps.com/v1.1/inspections/recent/placeid/$placeId?key=$apiKey" \
 
 while true
 do
