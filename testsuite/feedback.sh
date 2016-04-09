d0c7a7c5d9a49de386d274eec1445e4b400aecf9
Dino Morelli <dino@ui3.info>
2016-04-09 18:29:49 +0000

    Parameterized protocol, host, port
    This is to more easily test local and remote servers. Also removed
    pipe to json_reformat.

--- testsuite/feedback.sh
+++ testsuite/feedback.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+protocol="http"
+#protocol="https"
+
+hostPort="localhost:8610"
+#hostPort="ksdev.honuapps.com"
+
 apiKey=$(<$HOME/.config/ksnitch/test-apikey)
 feedback='{ "status": "New", "device_id": "a_test_device", "place_id": "ChIJtQic3uZXrIkRxIFU0wH2WWM", "date": 20160409, "issue_type": "Closed", "comment": "This restaurant is closed!" }'
 
@@ -7,5 +13,6 @@ curl \
    -v \
    --header "Content-Type: application/json" \
    --data "$feedback" \
-   "http://localhost:8610/v1.1/feedback?key=$apiKey" \
-   | json_reformat
+   "$protocol://$hostPort/v1.1/feedback?key=$apiKey"
+
+echo
