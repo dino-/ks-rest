e715bfd1a9ba3dba45bad0a1bbd27ff659ef0386
Dino Morelli <dino@ui3.info>
2017-09-04 14:46:34 +0000

    Moved Feedback data type from this project to ks-library
    Part of Kitchen Snitch #124

--- src/KS/Rest/Handler/Feedback.hs
+++ src/KS/Rest/Handler/Feedback.hs
@@ -13,13 +13,12 @@ module KS.Rest.Handler.Feedback
 
 import Control.Monad.Trans ( liftIO )
 import Control.Monad.Trans.Except ( ExceptT )
-import Data.Aeson ( FromJSON, ToJSON )
-import Data.Bson.Generic ( FromBSON ,ToBSON, fromBSON, toBSON )
+import Data.Bson.Generic ( toBSON )
 import Data.Pool ( Pool, withResource )
-import qualified Data.Text as T
 import Database.Mongo.Util ( lastStatus )
 import Database.MongoDB hiding ( options )
-import GHC.Generics ( Generic )
+import KS.Data.Feedback ( Feedback (..)
+   , IssueType (NoInspection, WrongNameAddr), Status (New) )
 import Servant ( NoContent (..), ServantErr )
 import Text.Printf ( printf )
 
@@ -27,63 +26,6 @@ import KS.Rest.APIKey ( akRead )
 import KS.Rest.Config ( Config (mongoConf), MongoConf (database) )
 import KS.Rest.Log ( infoM, lineM, lname )
 import KS.Rest.Util ( coll_feedback, requiredParam, verifyAPIKey )
-
-
-data Status = New | Duplicate | Resolved
-   deriving (Eq, Generic, Read, Show)
-
-instance FromJSON Status
-instance ToJSON Status
-
-instance FromBSON Status
-instance ToBSON Status
-
-
-data IssueType
-   = Closed | WrongNameAddr | InspectionNotCurrent | Other | NoInspection
-   deriving (Eq, Generic, Read, Show)
-
-instance FromJSON IssueType
-instance ToJSON IssueType
-
-instance FromBSON IssueType
-instance ToBSON IssueType
-
-
-data Feedback = Feedback
-   { status :: Status
-   , device_id :: T.Text
-   , place_id :: Maybe T.Text
-   , name :: Maybe T.Text
-   , date :: Int
-   , issue_type :: IssueType
-   , comment :: Maybe T.Text
-   }
-   deriving (Eq, Generic, Show)
-
-instance FromJSON Feedback
-instance ToJSON Feedback
-
-instance FromBSON Feedback where
-   fromBSON doc = Just $ Feedback
-      (read ("status" `at` doc))
-      ("device_id" `at` doc)
-      ("place_id" `at` doc)
-      ("name" `at` doc)
-      (read ("date" `at` doc))
-      (read ("issue_type" `at` doc))
-      ("comment" `at` doc)
-
-instance ToBSON Feedback where
-   toBSON adt =
-      [ "status" =: (show (status adt))
-      , "device_id" =: (device_id adt)
-      , "place_id" =: (place_id adt)
-      , "name" =: (name adt)
-      , "date" =: (show (date adt))
-      , "issue_type" =: (show (issue_type adt))
-      , "comment" =: (comment adt)
-      ]
 
 
 missingFeedback :: Feedback
