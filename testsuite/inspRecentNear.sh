15c42ed58e2518da9161f783e076152da3db9e17
Dino Morelli <dino@ui3.info>
2020-04-14 14:19:07 -0400

    Added some additional test host/port combos

--- testsuite/inspRecentNear.sh
+++ testsuite/inspRecentNear.sh
@@ -2,10 +2,14 @@
 
 apiKey=$(<$HOME/.config/ksnitch/test-apikey)
 
+hostPort="localhost:8610"      # local development
+# hostPort="ks-prod-01"     # AWS development
+# hostPort="honuapps.com"   # production
+
 # hq
 lat=35.8080294
 lng=-78.5604068
 
 dist=1000
 
-curl "http://localhost:8610/v1.1/inspections/recent/near?key=$apiKey&lat=$lat&lng=$lng&dist=$dist" | json_reformat
+curl "http://${hostPort}/v1.1/inspections/recent/near?key=${apiKey}&lat=${lat}&lng=${lng}&dist=${dist}" | json_reformat
